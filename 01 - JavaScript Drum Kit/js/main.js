8785c1d23c738e878d7a843790ff5fcfeb1b6e4c
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2017-08-24 14:44:24 +0200

    exercise01 finished

--- 01 - JavaScript Drum Kit/js/main.js
+++ 01 - JavaScript Drum Kit/js/main.js
@@ -1,13 +1,25 @@
-window.addEventListener("keydown", function(e){
+function removeTransition(e) {
+  if (e.propertyName != "transform") {
+    return;
+  }
+  this.classList.remove("playing");
+}
 
+function playSound(e) {
   var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  var key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
   // if you hit key that is not associated with data-key of audio elements, then stop function
   if (!audio) {
     return
   }
   // start playing audio from begening after every key hit
   audio.currentTime = 0;
-  
-  audio.play();
 
-})
+  audio.play();
+  key.classList.add("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(element => element.addEventListener('transitionend', removeTransition));
+window.addEventListener("keydown", playSound);
